d5936c0f35e877c63c43c128187e64ea94ea1ccb
alcl2000 <amy.555.lewis@gmail.com>
2023-10-20 15:04:49 +0000

    add: installed apps

--- back-end/app/app/settings.py
+++ back-end/app/app/settings.py
@@ -39,7 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
+    'rest_framework',
+    'django_filters',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
 ]
 
 MIDDLEWARE = [
