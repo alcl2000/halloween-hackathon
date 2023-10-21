9476c3da9125adbd7454f615e5df418f9b3d4530
Laurie C <133490867+lmcrean@users.noreply.github.com>
2023-10-21 17:58:15 +0100

    Create signup page

--- front-end/halloween-frontend/src/App.js
+++ front-end/halloween-frontend/src/App.js
@@ -1,14 +1,58 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+function SignUpForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Perform sign-up logic here
+    console.log('Form submitted:', { username, password });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Confirm Password:
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Edit <code>src/App.js</code> and save to reload. Hello World!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +61,9 @@ function App() {
         >
           Learn React
         </a>
+
+        <h1>Sign Up</h1>
+        <SignUpForm />
       </header>
     </div>
   );
