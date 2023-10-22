32f2feb7534ce8692f355f2d06aeb6f9289e6d96
MiaRasmussen05 <rasmussenm376@gmail.com>
2023-10-22 05:00:09 +0000

    Get front end up and running with bootstrap

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Button from "react-bootstrap/Button";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
+}
+
+export default App;
