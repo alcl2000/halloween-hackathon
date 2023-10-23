edb6eb7774d7d201c83f9c5495d2ad4dbc1bbb6c
alcl2000 <amy.555.lewis@gmail.com>
2023-10-23 11:25:27 +0000

    add: basic card details

--- src/components/Costume.js
+++ src/components/Costume.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Card } from 'react-bootstrap'
 
 const Costume = () => {
   return (
-    <div>Costume</div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
   )
 }
 
