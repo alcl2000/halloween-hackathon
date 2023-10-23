de503ec8be6992813b2632e56df78c1cab448548
alcl2000 <amy.555.lewis@gmail.com>
2023-10-23 10:40:41 +0000

    fix: backend moved to new repo

--- favorites/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('favorites/', views.FavoriteList.as_view()),
-    path('favorite-costumes/<int:pk>/', views.FavoriteDetail.as_view(), name='favorite-costume-detail'),
-    path('costume/<int:costume_id>/save/', views.FavoriteCostumePost.as_view(), name='favorite-costume-post'),
-
-]
